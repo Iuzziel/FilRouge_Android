5d6c6a2de55766426b15eeaa88611aa9d0ffdaf7
iuzziel <iuzziel@hotmail.fr>
2017-11-03 11:14:40 +0100

    push vendredi 11h
    Transformer la listMeteo en fragment, et l'afficher dans le detail evenement.

--- app/src/main/java/afpa/dl101_filrouge_android/vues/ListEvenementDetail.java
+++ app/src/main/java/afpa/dl101_filrouge_android/vues/ListEvenementDetail.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.concurrent.ExecutionException;
 
 import afpa.dl101_filrouge_android.R;
+import afpa.dl101_filrouge_android.metier.ToolBox;
 import afpa.dl101_filrouge_android.objet.Evenement;
 import afpa.dl101_filrouge_android.tacheAsynchrone.SelectEventFromDateAsync;
 import afpa.dl101_filrouge_android.tacheAsynchrone.SelectEventFromIdAsync;
@@ -27,6 +30,19 @@ public class ListEvenementDetail extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        TextView editTitre = (TextView) findViewById(R.id.editTitre);
+        TextView dateDebut = (TextView) findViewById(R.id.dateDebutDisplay);
+        TextView dateFin = (TextView) findViewById(R.id.dateFinDisplay);
+        TextView editEmpl = (TextView) findViewById(R.id.editEmpl);
+        TextView editDesc = (TextView) findViewById(R.id.editDescription);
+
+        editTitre.setText(evenement.getTitre());
+        dateDebut.setText(ToolBox.formatDate(String.valueOf(evenement.getDateIntDebut())));
+        dateFin.setText(ToolBox.formatDate(String.valueOf(evenement.getDateIntFin())));
+        editEmpl.setText(evenement.getLocation());
+        editDesc.setText(evenement.getDescription());
+
         Intent list = new Intent(this, ListMeteo.class);
         list.putExtra("locMeteo", evenement.getLocation());
         startActivity(list);
